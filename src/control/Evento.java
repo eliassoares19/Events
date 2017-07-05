6cd7d5d4672beea72862342ebc89d1dfdf884afb
eliassoares19 <eliassoares19@gmail.com>
2017-07-05 10:17:47 -0300

    Atributos da classe Evento

--- src/control/Evento.java
+++ src/control/Evento.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package control;
+
+import java.util.Date;
 
 /**
  *
  * @author elias.andrade
  */
 public class Evento {
-    
+    private int id;
+    private String tipo;
+    private int numConvidados;
+    private int tempoDuracao;
+    private String local;
+    private Date data;
+    private String descricao;
+    private int horaInicio;
+    private int horaTermino;
 }
