86568ecb2a4ea2d86114a2012c976ce9b12de32d
eliassoares19 <eliassoares19@gmail.com>
2017-07-05 09:35:55 -0300

    Adicionando algumas classes nao implementadas ainda

--- src/model/Aniversario.java
+++ src/model/Aniversario.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package events;
+package model;
 
 /**
  *
  * @author elias.andrade
  */
-public class Events {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Aniversario {
     
 }
---
+++ src/view/Events.java
@@ -0,0 +1,16 @@
+package view;
+
+/**
+ *
+ * @author elias.andrade
+ */
+public class Events {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
